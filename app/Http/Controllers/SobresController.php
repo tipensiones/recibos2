0647f4629f368ec55ee7195d0f16268c481f68d4
jezufi <jezufi@hotmail.com>
2024-10-03 15:05:30 -0600

    Revert "Update SobresController.php"
    This reverts commit 02bb4af37d98318a3bc731c8e8aa1547cc51f7a2.

--- app/Http/Controllers/SobresController.php
+++ app/Http/Controllers/SobresController.php
@@ -89,7 +89,6 @@ class SobresController extends Controller
                         ->where('tipo_nomina', $tipo_nomina)
                         ->where('jpp', $jpp)
                         ->where('numjpp', $numjpp)
-                        ->where('clave','>',60)
                         ->get();
             array_push($respaldos, $nomina);
         }
