dc107772e1b84f1c7a3611f10a65c754bd43752c
jezufi <jezufi@hotmail.com>
2024-10-03 14:44:39 -0600

    Update recibo.blade.php

--- resources/views/recibo.blade.php
+++ resources/views/recibo.blade.php
@@ -136,7 +136,7 @@
         </td>
         <td>
             <table>
-            @foreach (@respaldos as $item)
+            @foreach ($respaldos as $item)
                 @php
                     $deducciones = $deducciones + Arr::get($item, 'monto', 0);
                 @endphp
